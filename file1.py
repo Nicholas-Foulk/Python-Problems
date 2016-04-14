a5899d02339fce1902e20639aefef8a0a90bc03a
skillasaurus <nickfoulksjsu@gmail.com>
2016-04-13 19:42:54 -0700

    First python 3 file

---
+++ file1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+my_name = "Nicholas_Foulk"
+
+print (my_name)
+
+my_age = 22
+my_height = "5'10"
+my_haircolor = "brown"
+my_eyecolor = "brown"
+my_weight = 160
+	
+def print_method():
+	print(my_age)
+	print(my_height)
+	print(my_haircolor)
+	print(my_eyecolor)
+	print(my_weight)
+
+
+print_method()
+
+def EditPersonInfo(self,studID,**kwargs):
+	
